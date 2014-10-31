b263ccf8e52489c1204f48381558ae51ab6c6caa
Carrie O'Malley <carrie.omalley@gmail.com>
2014-10-31 10:40:26 +0000

    corrected some faulty logic in the routes; improved layout

--- routes/index.js
+++ routes/index.js
@@ -12,41 +12,33 @@ router.get('/', function(req, res) {
 router.post("/submit", function(req, res) {
     var results = [];
     var suite_id = req.body.suite_id;
-    request.get("http://testrail/index.php?/api/v2/get_suite/" + suite_id)
-        .type("json")
-        .auth("", "")
-        .end(function(suite_err, suite_res) {
-            if (suite_err) {
-                res.send(suite_err.message);
-            } else {
-                var project_id = suite_res.project_id;
-                request.get("http://testrail/index.php?/api/v2/get_cases/" + project_id + "/&" + suite_id)
-                    .auth("", "")
-                    .type("json")
-                    .end(function(cases_err, cases_res) {
-                        if (case_err) {
-                            res.send(case_err.message);
-                        }
-                        var case_ids = cases_res;
-                        case_ids.forEach(function(e, i, o) {
-                            var case_id = e.id;
-                            request.get("http://testrail/index.php?/api/v2/get_case/" + case_id)
-                                .auth("", "")
-                                .type("json")
-                                .end(function(case_err, case_res) {
-                                    if (cases_err) {
-                                        res.send(cases_err.message);
-                                    } else {
-                                        results.push(case_res);
-                                    }
-                                });
-                        });
-
-                        res.send(results);
-
-                    });
-            }
+    request.get("http://**/testrail/index.php?/api/v2/get_cases/1" + "&suite_id=" + suite_id)
+    .auth("*", "*")
+    .type("json")
+    .end(function(cases_err, cases_res) {
+        if (cases_err) {
+            res.send(case_err.message);
+        }
+        //console.log(cases_res.body);
+        var case_ids = cases_res.body;
+        case_ids.forEach(function(e, i, o) {
+            var case_id = e.id;
+            console.log(case_id);
+            request.get("http://**/testrail/index.php?/api/v2/get_case/" + case_id)
+            .auth("*", "*")
+            .type("json")
+            .end(function(case_err, case_res) {
+                console.log(case_res.text);
+                if (cases_err) {
+                    res.send(cases_err.message);
+                } else {
+                    results.push(case_res.text);
+                }
+            });
         });
+
+        res.send(results);
+    });
 });
 
 module.exports = router;
