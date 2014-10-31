ba4bd13801517624ea9589a53427b0617619f5ef
Carrie O'Malley <carrie.omalley@gmail.com>
2014-10-31 11:32:09 +0000

    the data is returned to the page in raw json instead of empty array

--- routes/index.js
+++ routes/index.js
@@ -12,32 +12,28 @@ router.get('/', function(req, res) {
 router.post("/submit", function(req, res) {
     var results = [];
     var suite_id = req.body.suite_id;
-    request.get("http://**/testrail/index.php?/api/v2/get_cases/1" + "&suite_id=" + suite_id)
-    .auth("*", "*")
+    request.get("http://url/testrail/index.php?/api/v2/get_cases/1" + "&suite_id=" + suite_id)
+    .auth("user", "pass")
     .type("json")
     .end(function(cases_err, cases_res) {
         if (cases_err) {
             res.send(case_err.message);
         }
-        //console.log(cases_res.body);
         var case_ids = cases_res.body;
         case_ids.forEach(function(e, i, o) {
             var case_id = e.id;
-            console.log(case_id);
-            request.get("http://**/testrail/index.php?/api/v2/get_case/" + case_id)
-            .auth("*", "*")
+            request.get("http://url/testrail/index.php?/api/v2/get_case/" + case_id)
+            .auth("user", "pass")
             .type("json")
             .end(function(case_err, case_res) {
-                console.log(case_res.text);
                 if (cases_err) {
                     res.send(cases_err.message);
                 } else {
                     results.push(case_res.text);
+                    if (results.length == case_ids.length) res.send(results);
                 }
             });
         });
-
-        res.send(results);
     });
 });
 
