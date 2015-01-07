d0091268d228fcc31efa657a7dc6c69596f76c22
Carrie O'Malley <carrie.omalley@gmail.com>
2015-01-07 16:22:37 +0000

    linting

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,19 @@ var router = express.Router();
 var request = require('superagent');
 
 /* GET home page */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.render('index', {
     title: 'Monorail'
   });
 });
 
-router.post('/submit', function(req, res, next) {
+router.post('/submit', function (req, res, next) {
   var results = [];
   var suite_id = req.body.suite_id;
   request.get('http://192.168.200.38/testrail/index.php?/api/v2/get_suite/' + suite_id)
   .auth('carrie.omalley@cascadehr.co.uk', 'm8tbsauh')
   .type('json')
-  .end(function(suite_err, suite_res) {
+  .end(function (suite_err, suite_res) {
     if (suite_err) {
       res.send(suite_err.message);
     }
@@ -23,21 +23,21 @@ router.post('/submit', function(req, res, next) {
     request.get('http://192.168.200.38/testrail/index.php?/api/v2/get_cases/1' + "&suite_id=" + suite_id)
     .auth('carrie.omalley@cascadehr.co.uk', 'm8tbsauh')
     .type('json')
-    .end(function(cases_err, cases_res) {
+    .end(function (cases_err, cases_res) {
       if (cases_err) {
         res.send(cases_err.message);
       }
       request.get('http://192.168.200.38/testrail/index.php?/api/v2/get_sections/1' + '&suite_id=' + suite_id)
       .auth('carrie.omalley@cascadehr.co.uk', 'm8tbsauh')
       .type('json')
-      .end(function(sections_err, sections_res) {
+      .end(function (sections_err, sections_res) {
         var case_ids = cases_res.body;
-        case_ids.forEach(function(e, i, o) {
+        case_ids.forEach(function (e, i, o) {
           var case_id = e.id;
           request.get('http://192.168.200.38/testrail/index.php?/api/v2/get_case/' + case_id)
           .auth('carrie.omalley@cascadehr.co.uk', 'm8tbsauh')
           .type('json')
-          .end(function(case_err, case_res) {
+          .end(function (case_err, case_res) {
             if (case_err) {
               res.send(case_err.message);
             } else {
@@ -47,17 +47,17 @@ router.post('/submit', function(req, res, next) {
               }
               results.push(result);
               if (results.length === case_ids.length) {
-                var dispOrder = sections_res.body.sort(function(a,b) {
+                var dispOrder = sections_res.body.sort(function (a,b) {
                   return a.id - b.id;
                 });
-                var sectionIds = dispOrder.map(function(e) {return e.id;});
-                var sectionPositions = dispOrder.map(function(e) {return e.display_order;});
-                results.forEach(function(e, i, o) {
+                var sectionIds = dispOrder.map(function (e) {return e.id;});
+                var sectionPositions = dispOrder.map(function (e) {return e.display_order;});
+                results.forEach(function (e, i, o) {
                   results[i].display_order = sectionPositions[sectionIds.indexOf(e.section_id)];
                 });
                 res.render('submit', {
                   title: 'Suite ' + suite_id + ': ' + suite_name,
-                  results: results.sort(function(a, b) {
+                  results: results.sort(function (a, b) {
                     return a.display_order - b.display_order;
                   })
                 });
@@ -70,7 +70,7 @@ router.post('/submit', function(req, res, next) {
   });
 });
 
-router.get("/test", function(req, res, next) {
+router.get("/test", function (req, res, next) {
   next({
     ErrorMessage: 'oh noes'
   });
